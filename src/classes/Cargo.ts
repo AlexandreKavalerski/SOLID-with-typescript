a7679d8f131fb1dc3a0dc222e61694479f1c0ab9
Alexandre Kavalerski <kavalerskialexandre@gmail.com>
2021-07-22 14:17:52 -0300

    feat(Cargo): add function getProximoCargo

--- src/classes/Cargo.ts
+++ src/classes/Cargo.ts
@@ -5,4 +5,11 @@ enum Cargo {
   GERENTE,
 }
 
+export function getProximoCargo(cargoAtual: number): Cargo {
+  if (cargoAtual !== Cargo.GERENTE) {
+    return cargoAtual + 1;
+  }
+  return cargoAtual;
+}
+
 export default Cargo;
