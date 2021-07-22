53c3150985245d3af79870a9a775a537c59d2fdb
Alexandre Kavalerski <kavalerskialexandre@gmail.com>
2021-07-22 09:20:43 -0300

    feat: add date check to Reajuste

--- src/services/ReajusteService.ts
+++ src/services/ReajusteService.ts
@@ -8,6 +8,19 @@ export default class ReajusteService {
     if (percentualReajuste > 0.4) {
       throw new Error("O reajuste não pode ser maior que 40% do salário");
     }
+    const dataUltimoReajuste: Date = funcionario.getDataUltimoReajuste();
+    const dataAtual: Date = new Date();
+
+    const mesesDesdeUltimoReajuste: number =
+      dataUltimoReajuste.getMonth() -
+      dataAtual.getMonth() +
+      12 * (dataUltimoReajuste.getFullYear() - dataAtual.getFullYear());
+    if (mesesDesdeUltimoReajuste < 6) {
+      throw new Error(
+        "O reajuste não pode acontecer em um intervalo menor que 6 meses"
+      );
+    }
+
     const salarioReajustado = salarioAtual + aumento;
 
     funcionario.reajustarSalario(salarioReajustado);
