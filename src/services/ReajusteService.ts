07067cd11ef38a3d699ef4ca01cf7ade01d281d0
Alexandre Kavalerski <kavalerskialexandre@gmail.com>
2021-07-22 09:42:29 -0300

    refactor: applying Open Closed Principle

--- src/services/ReajusteService.ts
+++ src/services/ReajusteService.ts
@@ -1,28 +1,17 @@
 import Funcionario from "../classes/Funcionario";
+import ValidacaoReajuste from "./ValidacaoReajuste";
 
 export default class ReajusteService {
+  private validacoes: ValidacaoReajuste[];
+
+  constructor(validacoes: ValidacaoReajuste[]) {
+    this.validacoes = validacoes;
+  }
+
   reajustarSalarioDoFuncionario(funcionario: Funcionario, aumento: number) {
-    const salarioAtual = funcionario.getSalario();
+    this.validacoes.forEach((v) => v.validar(funcionario, aumento));
 
-    const percentualReajuste = aumento / salarioAtual;
-    if (percentualReajuste > 0.4) {
-      throw new Error("O reajuste não pode ser maior que 40% do salário");
-    }
-    const dataUltimoReajuste: Date = funcionario.getDataUltimoReajuste();
-    const dataAtual: Date = new Date();
-
-    const mesesDesdeUltimoReajuste: number =
-      dataUltimoReajuste.getMonth() -
-      dataAtual.getMonth() +
-      12 * (dataUltimoReajuste.getFullYear() - dataAtual.getFullYear());
-    if (mesesDesdeUltimoReajuste < 6) {
-      throw new Error(
-        "O reajuste não pode acontecer em um intervalo menor que 6 meses"
-      );
-    }
-
-    const salarioReajustado = salarioAtual + aumento;
-
+    const salarioReajustado = funcionario.getSalario() + aumento;
     funcionario.reajustarSalario(salarioReajustado);
   }
 }
